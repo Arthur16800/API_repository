dab2025c82d1ada6d625caf05a8465537763d727
Arthur16800 <arthur08coutinho>
2024-09-18 09:38:39 -0300

    Correção do dia 18_09

---
+++ correcaoParPrimoArthur/src/controllers/controllerNumbers.js
@@ -0,0 +1,25 @@
+module.exports = class controllerNumbers {
+  static async checkPar(req, res) {
+    // req = requisição ; res = resposta
+    const { numero } = req.body; // esse body é executado no postman, mais para frente sera no html(ex:formulario)
+
+    if (numero % 2 == 0) {
+      return res.status(200).json({ message: "Número par!" });
+    }
+    return res.status(200).json({ message: "Número ímpar!" });
+  }
+
+  static async checkPrimo(req, res) {
+    const {numero} = req.body;
+
+    if(numero <= 1){
+        return res.status(200).json({message: "Não é primo"});
+    }
+    for(let i = 2; i < numero; i++)// ele vai dividindo o numero por valores menores que ele até chegar nele 
+        if(numero % i == 0){
+            return res.status(200).json({message: "Não é primo"});
+        }
+
+    return res.status(200).json({message: "Número primo"});
+  }
+};
---
+++ correcaoParPrimoArthur/src/routes/apiRoutes.js
@@ -0,0 +1,10 @@
+//Importa o módulo Router do express
+//Router será utilizada para definir rotas específicas
+const router = require('express').Router();
+
+const controllerNumbers = require('../controllers/controllerNumbers'); // esse .. significa que estou voltando uma pasta(cd ..)
+
+router.post('/par/', controllerNumbers.checkPar); // http://localhost:5000/arthurparprimo/par <-- esse /par é um endpoint 
+router.post('/primo/', controllerNumbers.checkPrimo);
+
+module.exports = router
