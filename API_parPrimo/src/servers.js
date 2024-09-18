aef80b0dbef719d3d6c68878f62ee16d21ada9e5
Arthur16800 <arthur08coutinho@gmail.com>
2024-09-18 12:59:27 -0300

    .

--- API_parPrimo/src/servers.js
+++ API_parPrimo/src/servers.js
@@ -2,5 +2,5 @@
 const app = require("./index");
 
 // Inicia o servidor na porta 5000 da máquina
-// Neste caso como esta local a API será acessível em http://localhost:5000/
+// Neste caso como esta local a API será acessível em http://localhost:5000/ /
 app.listen(5000);
